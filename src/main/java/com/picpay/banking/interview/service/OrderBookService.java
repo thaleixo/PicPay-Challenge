95e4f0b6912210db403660aad94b7ea7535da9fd
Thiago Henrique <thiagohenriquealeixo1@Gmail.com>
2025-03-30 17:54:23 -0300

    feat(order) add new methods in interface

--- src/main/java/com/picpay/banking/interview/service/OrderBookService.java
+++ src/main/java/com/picpay/banking/interview/service/OrderBookService.java
@@ -3,10 +3,18 @@ package com.picpay.banking.interview.service;
 import com.picpay.banking.interview.domain.Order;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderBookService {
     long calculateTotalBuyOrders(List<Order> orderBook);
     long calculateTotalSellOrders(List<Order> orderBook);
     double calculateTotalValueOrders(List<Order> orderBook);
     long calculateTotalQuantityOrders(List<Order> orderBook);
+    Order createOrder(Order order);
+    Optional<Order> getOrderById(Integer id);
+    List<Order> getAllOrders();
+    List<Order> getOrdersBySymbol(String symbol);
+    boolean validateQuantityOrderBook(Order order);
+    void deleteOrderById(Integer id);
+    boolean existsById(Integer id);
 }
