0fe468e5c74d4e2c765ae1f36b53d20333f971ee
Thiago Henrique <thiagohenriquealeixo1@Gmail.com>
2025-03-30 18:37:23 -0300

    fix(tests) fix orderBookControllerTest name

--- src/test/java/com/picpay/banking/interview/service/impl/OrderBookControllerTest.java
+++ src/test/java/com/picpay/banking/interview/service/impl/OrderBookControllerTest.java
@@ -27,7 +27,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class OrderControllerTest {
+class OrderBookControllerTest {
 
     @Mock
     private OrderBookService orderService;
