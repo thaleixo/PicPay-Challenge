09523ac12ca5c44becbd143be0f6adabb93b0ba7
Thiago Henrique <thiagohenriquealeixo1@Gmail.com>
2025-03-30 17:53:56 -0300

    feat(order) add new methods in interface

--- src/main/java/com/picpay/banking/interview/service/impl/OrderBookServiceImpl.java
+++ src/main/java/com/picpay/banking/interview/service/impl/OrderBookServiceImpl.java
@@ -1,33 +1,88 @@
 package com.picpay.banking.interview.service.impl;
 
 import com.picpay.banking.interview.domain.Order;
+import com.picpay.banking.interview.domain.enumeration.Side;
+import com.picpay.banking.interview.domain.repository.OrderRepository;
+import com.picpay.banking.interview.exceptions.InvalidOrderException;
 import com.picpay.banking.interview.service.OrderBookService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class OrderBookServiceImpl implements OrderBookService {
+
+    private final OrderRepository orderRepository;
 
     @Override
     public long calculateTotalBuyOrders(List<Order> orderBook) {
-        return 0;
+        return orderBook.stream()
+                .filter(order -> order.getSide() == Side.BUY)
+                .count();
     }
 
     @Override
     public long calculateTotalSellOrders(List<Order> orderBook) {
-        return 0;
+        return orderBook.stream()
+                .filter(order -> order.getSide() == Side.SELL)
+                .count();
     }
 
     @Override
-    public double calculateTotalValueOrders(
-            List<Order> orderBook) {
-        return 0;
+    public double calculateTotalValueOrders(List<Order> orderBook) {
+        return orderBook.stream()
+                .mapToDouble(order -> order.getPrice().doubleValue())
+                .sum();
     }
 
     @Override
-    public long calculateTotalQuantityOrders(
-            List<Order> orderBook) {
-        return 0;
+    public long calculateTotalQuantityOrders(List<Order> orderBook) {
+        return orderBook.stream()
+                .mapToLong(Order::getQuantity)
+                .sum();
+    }
+
+    @Override
+    @Transactional
+    public Order createOrder(Order order) {
+        return orderRepository.save(order);
+    }
+
+    @Override
+    public Optional<Order> getOrderById(Integer id) {
+        return orderRepository.findById(id);
+    }
+
+    @Override
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    @Override
+    public List<Order> getOrdersBySymbol(String symbol) {
+        return orderRepository.findBySymbol(symbol);
+    }
+
+    @Override
+    public boolean validateQuantityOrderBook(Order order) {
+        return order.getQuantity() >= 2;
+    }
+
+    @Override
+    public void deleteOrderById(Integer id) {
+        Optional<Order> order = orderRepository.findById(id);
+        if (order.isPresent()){
+            orderRepository.delete(order.get());
+        }
+
+    }
+
+    @Override
+    public boolean existsById(Integer id) {
+        return orderRepository.existsById(id);
     }
 }
