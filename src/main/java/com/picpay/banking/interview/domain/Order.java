6aa5c1dd1caa47f1b51cb5dee88a24b4f4556778
Thiago Henrique <thiagohenriquealeixo1@Gmail.com>
2025-03-30 17:57:58 -0300

    refactor(order): transform record into JPA entity class - Add @Entity, @Id and other JPA annotations

--- src/main/java/com/picpay/banking/interview/domain/Order.java
+++ src/main/java/com/picpay/banking/interview/domain/Order.java
@@ -1,51 +1,84 @@
 package com.picpay.banking.interview.domain;
 
 import com.picpay.banking.interview.domain.enumeration.Side;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
-public record Order (
-    String symbol,
-    int quantity,
-    BigDecimal price,
-    Side side
-) {
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 10)
+    private String symbol;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false, precision = 19, scale = 4)
+    private BigDecimal price;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 4)
+    private Side side;
+
+    @Builder
+    public Order(Integer id, String symbol, int quantity, BigDecimal price, Side side) {
+        this.id = id;
+        this.symbol = symbol;
+        this.quantity = quantity;
+        this.price = price;
+        this.side = side;
+    }
 
     public static OrderBuilder builder() {
         return new OrderBuilder();
     }
 
     public static class OrderBuilder {
-
+        private Integer id;
         private String symbol;
         private int quantity;
         private BigDecimal price;
         private Side side;
 
-        public OrderBuilder() {}
-
-        public OrderBuilder symbol(final String symbol) {
+        public OrderBuilder symbol(String symbol) {
             this.symbol = symbol;
             return this;
         }
 
-        public OrderBuilder quantity(final int quantity) {
+        public OrderBuilder quantity(int quantity) {
             this.quantity = quantity;
             return this;
         }
 
-        public OrderBuilder price(final BigDecimal price) {
+        public OrderBuilder price(BigDecimal price) {
             this.price = price;
             return this;
         }
 
-        public OrderBuilder side(final Side side) {
+        public OrderBuilder side(Side side) {
             this.side = side;
             return this;
         }
 
+        public OrderBuilder id(Integer id) {
+            this.id = id;
+            return this;
+        }
+
         public Order build() {
-            return new Order(symbol, quantity, price, side);
+            return new Order(id, symbol, quantity, price, side);
         }
     }
 }
